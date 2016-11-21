5c3a87f06404ba958e0389fc20bb3034dc084e17
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-21 22:11:02 +0900

    edit config/routes.rb
    add “end” to avoid errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups, only: :new do
     resources :messages, only: :index
+  end
   root to: "messages#index"
 end
