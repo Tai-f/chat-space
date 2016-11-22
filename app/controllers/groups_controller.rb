ade931bcf0cca9315de62946265348ea16f0003b
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-22 17:19:50 +0900

    Define "create" method
    1. define “create” method
    2. configure strong parameters

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,14 @@
 class GroupsController < ApplicationController
   def new
   end
+
+  def create
+    Group.create(create_params)
+    redirect_to controller: :messages, action: :index
+  end
+
+  protected
+    def create_params
+      params.permit( :name)
+    end
 end
