49f898d45c0102a8ae12f214df17e86aa7cd5cc9
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-22 17:17:31 +0900

    Configure routes of "groups"
    Add “create” action of groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :groups, only: :new do
+  resources :groups, only: [:new, :create] do
     resources :messages, only: :index
   end
   root to: "messages#index"
