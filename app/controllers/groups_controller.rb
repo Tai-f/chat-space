48c4383c162f22cf806feb501704aff719d2e5dd
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-23 16:46:25 +0900

    Modify groups_controller.rb
    add if method by @group.save

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,8 +4,8 @@ class GroupsController < ApplicationController
   end
 
   def create
-    group = Group.create(create_params)
-    if group.present?
+    @group = Group.new(create_params)
+    if @group.save
       redirect_to controller: :messages, action: :index
     else
       render action: :new
