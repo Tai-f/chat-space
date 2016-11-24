3c473b2aa3886ebb02c3672a98ba80e9c8dc4cd1
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-24 17:10:26 +0900

    Use "accepts_nested_attributes_for" method
    Add accepts_nested_attributes_for :group_users

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,8 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
+
+  accepts_nested_attributes_for :group_users
+
   validates :name, presence: true
 end
