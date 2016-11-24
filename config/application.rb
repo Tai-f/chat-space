e838785ce24d7b5248258269841601fa48018750
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-24 20:55:34 +0900

    Set not to invoke some files which don't need

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,11 @@ module ChatSpace
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
     config.generators do|g|
-      g.assets false
+      g.stylesheets false
+      g.javascripts false
       g.helper false
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+
 
   end
 end
