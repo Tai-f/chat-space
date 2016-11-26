1b14134bee7dc4bc7ddb1cae25ed4b83df0f1f90
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-26 17:50:42 +0900

    Add "if" method in "create" method
    it makes 2 cases that @messages saved or not

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,13 +6,18 @@ class MessagesController < ApplicationController
   end
 
   def create
-    Message.create(message_params)
+    @message = Message.new(message_params)
+    if @message.save
     redirect_to action: :index
+    else
+      render action: :index
+    end
   end
 
   private
   def message_params
     params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
+
 
 end
