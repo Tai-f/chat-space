d1eff364c56150f62d1451f294dfbb069dca0d05
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-26 17:15:08 +0900

    Set "groups#index" to the root

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-  root to: "messages#index"
+  root to: "groups#index"
 end
