00e385b2d822c7f971f6b9cb0fa61a8455293a3b
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-28 16:33:17 +0900

    Modify the key of the message
    because the key“email” is not existed

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,6 +3,8 @@ describe Message do
   describe '#create' do
 
     it "is valid with a body, email, group_id, user_id" do
+      message = create(:message)
+      expect(message).to be_valid
     end
   end
 end
