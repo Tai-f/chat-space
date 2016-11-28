ca3d200fc68e91bb5914c69829d8c71553473e75
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-28 21:12:56 +0900

    Modify the value of "image"
    it used  “Avatar” of “Faker”

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :message do
     body  {Faker::Beer}
-    image           "1.png"
+    image {Faker::Avatar}
     group_id        "5"
     user_id         "4"
   end
