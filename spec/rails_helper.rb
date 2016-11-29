c39d18dc502f8e5d9856be49349c4daba694884d
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-29 12:16:21 +0900

    Add codes to use in Factorygirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,10 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.include ActionDispatch::TestProcess
+
+  FactoryGirl::SyntaxRunner.class_eval do
+    include ActionDispatch::TestProcess
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
