cb3fb9b322605c9c369b840b681761face9b1702
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-29 14:20:45 +0900

    Configure "image" to upload files
    configure the path of the image files

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :message do
     body  {Faker::Beer}
-    image {Faker::Avatar}
+    image { fixture_file_upload("#{::Rails.root}/spec/fixtures/image/1.png")}
     group_id        "5"
     user_id         "4"
   end
