fbed774bd5bc354066a4a2bebdce3bfb1674981a
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-29 19:51:50 +0900

    Describe codes whether it renders correctly in "index"

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe MessagesController do
   describe 'GET #index' do
     it "renders the :index template" do
+      get :index, use_route: :messages
+      expect(response).to render_template :index
     end
 
   end
