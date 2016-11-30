a56b1e42cf32ec07565f1f989f184e406a06f5ee
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-30 11:30:07 +0900

    Configured by these codes if i use "assigns" method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,12 @@ RSpec.configure do |config|
   end
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
