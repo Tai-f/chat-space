2b1959570c6a01cc580b81193409af09a45d53ac
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-30 16:36:21 +0900

    Describe expectations of saving correlctly
    modify some codes of  “index” to confirm routes

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,16 +3,18 @@ require 'rails_helper'
 describe MessagesController do
   describe 'GET #index' do
     it "renders the :index template" do
-      get :index, use_route: :messages
+      get :index, group_id: 5
       expect(response).to render_template :index
     end
   end
 
-
-
   describe 'POST #create' do
     it "saves the new contact in the database" do
+      expect{
+        post :create, group_id: 5, message: attributes_for(:message)
+      }.to change(Message, :count).by(1)
     end
+
 
   end
 end
