4c982d23e198d244b0e25383037c987b7da4a9c9
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-11-30 16:30:32 +0900

    Modify some codes
    It changed from string to integer.
    set to the kind of image.

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
 
   factory :message do
     body  {Faker::Beer}
-    image { fixture_file_upload("#{::Rails.root}/spec/fixtures/image/1.png")}
-    group_id        "5"
-    user_id         "4"
+    image { fixture_file_upload("#{::Rails.root}/spec/fixtures/image/1.png", "image/png")}
+    group_id        5
+    user_id         4
   end
 
 end
