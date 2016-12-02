5999344504fac973e4206fc4f718cb836a8c50be
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-02 18:19:01 +0900

    Define "appendList" function
    define each method to add data

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -3,5 +3,16 @@ $(function(){
   var textField = $("#message_body");
   var messageUl = $(".chat-messages");
 
-  
+  function appendList(data){
+    var dateElement = $('<p class = "chat-message__time">')
+    var messageElement = $('<p class= "chat-message__body">')
+    var listElement = $('<li class= "chat-message">')
+    var name = $("<h5>").text();
+    var appendName = $('<p class= "chat-message__name">').append(name);
+    var appendDate = dateElement.append(data.created_at);
+    var appendMessage = messageElement.append(data.body);
+    var appendList = listElement.append(appendName).append(appendDate).append(appendMessage);
+    messageUl.append(appendList)
+  }
+
 });
