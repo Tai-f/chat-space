bf321c420a4aaf10f61aa29470ae9dfd3764334d
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-03 12:03:58 +0900

    Add some codes

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -4,15 +4,17 @@ $(function(){
   var messageUl = $(".chat-messages");
 
   function appendList(data){
-    var dateElement = $('<p class = "chat-message__time">')
-    var messageElement = $('<p class= "chat-message__body">')
-    var listElement = $('<li class= "chat-message">')
-    var name = $("<h5>").text();
+    var dateElement = $('<p class = "chat-message__time">');
+    var messageElement = $('<p class= "chat-message__body">');
+    var listElement = $('<li class= "chat-message">');
+    var headerElement = $('<div class = "chat-message__header clearfix">');
+    var name = $('<h5>').text();
     var appendName = $('<p class= "chat-message__name">').append(name);
     var appendDate = dateElement.append(data.created_at);
     var appendMessage = messageElement.append(data.body);
-    var appendList = listElement.append(appendName).append(appendDate).append(appendMessage);
-    messageUl.prepend(appendList)
+    var appendHeader = headerElement.append(appendName).append(appendDate);
+    var appendList = listElement.append(appendHeader).append(appendMessage);
+    messageUl.prepend(appendList);
   }
 
   $("#new_message").submit(function(e){
