659b07414cef040b4b0e52ca3b414b091972d653
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-07 11:28:49 +0900

    Define "appendMembers" to search users

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,9 +1,15 @@
 $(function(){
 
-  $(".chat-group-form__input").on("keyup", function(e){
-    e.preventDefault();
+  var members = $("#user-search-result");
 
 
+  function appendMembers(data){
+    var nameElement = $('<p class="chat-group-user__name">').append(data.name);
+    var userAttr = {user_id: data.id, user_name: data.name};
+    var btnElement = $('<a class ="chat-group-user__btn chat-group-user__btn--add">').append("追加").attr(userAttr);
+    var appendUser = $('<div class="chat-group-user clearFix">').append(nameElement).append(btnElement);
+    members.append(appendUser);
+  }
 
-  });
+
 });
