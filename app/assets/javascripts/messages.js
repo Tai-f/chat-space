6cd1eb6660db06e870ea9b16770b34549dc1e750
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-09 14:04:21 +0900

    Modify the codes for sending the data of the form & the view of the image

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,42 +2,41 @@ $(function(){
 
   var textField = $("#message_body");
   var messageUl = $(".chat-messages");
+  var imageField = $("#message_image");
 
   function appendList(data){
-    var dateElement = $('<p class = "chat-message__time">');
-    var messageElement = $('<p class= "chat-message__body">');
-    var listElement = $('<li class= "chat-message">');
-    var headerElement = $('<div class = "chat-message__header clearfix">');
     var name = $('h5').text();
     var appendName = $('<p class= "chat-message__name">').append(name);
-    var appendDate = dateElement.append(data.created_at);
-    var appendMessage = messageElement.append(data.body);
-    var appendHeader = headerElement.append(appendName).append(appendDate);
-    var appendList = listElement.append(appendHeader).append(appendMessage);
+    var appendDate = $('<p class = "chat-message__time">').append(data.created_at);
+    var appendMessage = $('<p class= "chat-message__body">').append(data.body);
+    var appendImage = $('<img class="chat-message__image">').append(data.image);
+    var appendHeader = $('<div class = "chat-message__header clearfix">').append(appendName).append(appendDate);
+    var appendList = $('<li class= "chat-message">').append(appendHeader).append(appendMessage).append(appendImage);
     messageUl.prepend(appendList);
   }
 
-  $("#new_message").submit(function(e){
-    e.preventDefault();
-    var input = textField.val();
-
-    $.ajax({
-      type: 'POST',
-      url: './messages',
-      data: {
-        message: {
-          body: input, image: ""
-        }
-      },
-      dataType: "json"
-    })
-    .done(function(data){
-      appendList(data);
-      $("#submit").prop('disabled', false);
-      textField.val("");
-    })
-    .fail(function(){
-      alert("通信失敗しました")
+    $("#new_message").submit(function(){
+      var formData = new FormData($(this)[0]);
+      console.log(formData);
+      // var input = textField.val();
+      // var inputFile = imageField.prop("files")[0];
+      // formData.append(textField.attr('name'), input);
+      // formData.append(imageField.attr('name'),inputFile);
+      $.ajax({
+        type: 'POST',
+        url: './messages',
+        data: formData,
+        processData: false,
+        contentType: false,
+        dataType: "json"
+      })
+      .done(function(data){
+        appendList(data);
+        $("#submit").prop('disabled', false);
+        textField.val("");
+      })
+      .fail(function(){
+        alert("通信失敗しました")
+      });
     });
-  });
 });
